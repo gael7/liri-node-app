0981af326844dd4dbf00d19a9346d0732ec84887
Gael Arrambide <gael_arrambide@hotmail.com>
2016-09-29 13:29:08 -0500

    spotify working default and one word

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var data=require('./keys.js');
 var keys=data.twitterKeys;
 var Twitter = require('twitter');
 var client=new Twitter(keys);
+var spotify = require('spotify');
 
 switch(process.argv[2]){
   case 'my-tweets':
@@ -14,7 +15,20 @@ switch(process.argv[2]){
   break;
 
   case 'spotify-this-song':
+  if (process.argv[3]===undefined) {
+    console.log("'The Sign' by Ace of Base");
+  } else {
+  spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    var track=data.tracks.items;
+    console.log("Artist(s): "+track[0].artists[0].name +"\nSong: "+ track[0].name +"\nPreview: "+ track[0].preview_url +"\nAlbum: "+ track[0].album.name);
+  });
+}
   break;
+
   case 'movie-this':
   break;
   case 'do-what-it-says':
